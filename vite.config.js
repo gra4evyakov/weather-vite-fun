cbce8e6cc9082cda498c640d07de6a38b2e9ff9b
Grachev Yakov <gra4evyakov@yandex.ru>
2023-02-22 19:22:40 +0300

    init commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/weather-vite/',
+  base: '/weather-vite-fun/',
   plugins: [vue()],
   resolve: {
     alias: {
